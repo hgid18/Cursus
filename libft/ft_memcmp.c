14c9f759b9dae6aed63d75ece02a62b4301b334c
hgid18 <hgid18@gmail.com>
2025-11-10 20:07:12 +0100

    Add files via upload

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgarcia2 <hgarcia2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 15:39:47 by hgarcia2          #+#    #+#             */
+/*   Updated: 2025/11/10 19:59:15 by hgarcia2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t		i;
+	const char	*st1;
+	const char	*st2;
+
+	i = 0;
+	st1 = (const char *)s1;
+	st2 = (const char *)s2;
+	while (st1[i] && st2[i] && i < n)
+	{
+		if (st1[i] != st2[i])
+			return (st1[i] - st2[i]);
+		i++;
+	}
+	return (st1[i] - st2[i]);
+}
---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgarcia2 <hgarcia2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 16:03:07 by hgarcia2          #+#    #+#             */
+/*   Updated: 2025/11/10 16:07:14 by hgarcia2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size > 0)
+	{
+		while (src[i] && i < size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
