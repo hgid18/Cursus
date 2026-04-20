11c6b242916d2cd93735b3f862a1eff0f7cb89c5
hgid18 <hgid18@gmail.com>
2026-04-20 16:38:01 +0200

    Delete M2/Py01 directory

--- M2/Py01/ex0/ft_garden_intro.py
+++
@@ -1,9 +0,0 @@
-def ft_garden_intro():
-    print("=== Welcome to My Garden ===")
-    name = "Rose"
-    height = "25cm"
-    age = "30 days"
-    print(f"Plant: {name}")
-    print(f"Height: {height}")
-    print(f"Age: {age}")
-    print("\n=== End of program ===")
